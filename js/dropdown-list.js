6cfb2eca5dad3708724882b76d7bb3cde0b89731
Niki <gavrilovnikita2004@gmail.com>
2024-02-23 10:01:36 +0300

    changed couple margins for titles and added some quick changes for dropdown-list

--- js/dropdown-list.js
+++ js/dropdown-list.js
@@ -3,8 +3,10 @@ $(document).ready(function () {
   $("#show-full").click(function () {
     if (!flag) {
       $(".item-hidden").slideDown();
+      $(this).text("Свернуть");
     } else {
       $(".item-hidden").slideUp();
+      $(this).text("Показать полностью");
     }
     flag = !flag;
   });
